9881fd362e6e4a30da5440246c3fcba50c1ad639
Reofard <6590f1@naver.com>
2022-07-14 03:29:48 +0900

    18번 줄 수정7

--- html/sbadmin/act/download.php
+++ html/sbadmin/act/download.php
@@ -15,11 +15,11 @@
     header("Content-Type: application/octet-stream");
     header("Content-Disposition: attachment; filename={$row['upName']}");
 
-    if(strpos($row['upPath'],'../uploads/')===0)
-    {
-        $temp = preg_replace('../uploads/','',$row['upPath'])
-        if(strpos($temp,'..')===false && strpos($temp,'/')===false && strpos($temp,'\\')===false)
-        {
+    // if(strpos($row['upPath'],'../uploads/')===0)
+    // {
+        // $temp = preg_replace('../uploads/','',$row['upPath'])
+        // if(strpos($temp,'..')===false && strpos($temp,'/')===false && strpos($temp,'\\')===false)
+        // {
     //        echo file_get_contents($row['upPath']);
-        }
-    }
+        // }
+    // }
