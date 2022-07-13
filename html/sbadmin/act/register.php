815ba7b0d32dcbab70e6f296f33109be7625f366
Reofard <6590f1@naver.com>
2022-07-13 21:51:14 +0900

    18번 줄 수정

--- html/sbadmin/act/register.php
+++ html/sbadmin/act/register.php
@@ -4,6 +4,24 @@
     $email = $_POST['email'];
     $password = $_POST['password'];
     
+    $num = preg_match('/[0-9]/u', $password);
+    $eng = preg_match('/[a-z]/u', $password);
+    $spe = preg_match("/[\!\@\#\$\%\^\&\*]/u",$password);
+ 
+    if(strlen($password) < 10 || strlen($password) > 30)
+    {
+        die("<script>alert('비밀번호는 영문, 숫자, 특수문자를 혼합하여 최소 10자리 ~ 최대 30자리 이내로 입력해주세요.');window.location.href='../';</script>");
+    }
+ 
+    if(preg_match("/\s/u", $password) == true)
+    {
+        die("<script>alert('비밀번호는 공백없이 입력해주세요.');window.location.href='../';</script>");
+    }
+ 
+    if( $num == 0 || $eng == 0 || $spe == 0)
+    {
+        die("<script>alert('영문, 숫자, 특수문자를 혼합하여 입력해주세요.');window.location.href='../';</script>");
+    }
     $sql = "insert into users (email, password) values (?, ?)";
     $stmt = $db->stmt_init();
     $stmt->prepare($sql);
